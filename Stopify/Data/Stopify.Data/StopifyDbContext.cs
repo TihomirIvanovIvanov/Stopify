ab2acd613808ed072f31dac02cd94e4eefc6586a
Tihomir Ivanov <tihomir_2005@abv.bg>
2020-02-23 16:38:57 +0200

    Add database layer. Add Stopify.Web: fixed layout and some of the views, scaffold login, logout and register. Implement register functionality. Add service scope with creating for roles (admin, user).

---
+++ Stopify/Data/Stopify.Data/StopifyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Stopify.Data.Models;
+
+namespace Stopify.Data
+{
+    public class StopifyDbContext : IdentityDbContext<StopifyUser, IdentityRole, string>
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public StopifyDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Stopify/Web/Stopify.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Stopify.Data.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stopify.Web.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<StopifyUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<StopifyUser> _userManager;
+
+        public RegisterModel(
+            UserManager<StopifyUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            SignInManager<StopifyUser> signInManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [Display(Name = "Username")]
+            public string Username { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+        }
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl = "/Identity/Account/Login";
+
+            if (ModelState.IsValid)
+            {
+                var isRoot = _userManager.Users.Any();
+                var user = new StopifyUser { UserName = Input.Username, Email = Input.Email };
+                var result = await _userManager.CreateAsync(user, Input.Password);
+
+                if (result.Succeeded)
+                {
+                    if (isRoot)
+                    {
+                        await _userManager.AddToRoleAsync(user, "Admin");
+                    }
+
+                    return LocalRedirect(returnUrl);
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return Page();
+        }
+    }
+}
