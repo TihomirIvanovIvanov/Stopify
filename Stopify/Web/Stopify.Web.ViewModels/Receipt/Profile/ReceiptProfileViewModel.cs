ecbfbb430a1feef5b192b962e0a67a527604279e
Tihomir Ivanov <tihomir_2005@abv.bg>
2020-02-29 23:20:50 +0200

    Implement profile functionality and add the view

---
+++ Stopify/Web/Stopify.Web.ViewModels/Receipt/Profile/ReceiptProfileViewModel.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Stopify.Services.Mapping;
+using Stopify.Services.Models;
+using System;
+using System.Linq;
+
+namespace Stopify.Web.ViewModels.Receipt.Profile
+{
+    public class ReceiptProfileViewModel : IMapFrom<ReceiptServiceModel>, IHaveCustomMappings
+    {
+        public string Id { get; set; }
+
+        public DateTime IssuedOn { get; set; }
+
+        public decimal Total { get; set; }
+
+        public int Products { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration
+                .CreateMap<ReceiptServiceModel, ReceiptProfileViewModel>()
+                    .ForMember(destination => destination.Total,
+                                options => options.MapFrom(origin =>
+                               origin.Orders.Sum(order => order.Product.Price * order.Quantity)))
+                    .ForMember(destination => destination.Products,
+                                options => options.MapFrom(origin => 
+                               origin.Orders.Sum(order => order.Quantity)));
+        }
+    }
+}
--- Stopify/Web/Stopify.Web/Controllers/ReceiptController.cs
+++ Stopify/Web/Stopify.Web/Controllers/ReceiptController.cs
@@ -3,7 +3,9 @@ using Microsoft.EntityFrameworkCore;
 using Stopify.Services;
 using Stopify.Services.Mapping;
 using Stopify.Web.ViewModels.Receipt.Details;
+using Stopify.Web.ViewModels.Receipt.Profile;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Stopify.Web.Controllers
@@ -15,6 +17,19 @@ namespace Stopify.Web.Controllers
         public ReceiptController(IReceiptService receiptService)
         {
             this.receiptService = receiptService;
+        }
+
+        public async Task<IActionResult> Profile()
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            var receiptFromDb = await this.receiptService
+                .GetAllByRecipientId(userId).ToListAsync();
+
+            var receiptsForCurrentUser = receiptFromDb.Select(receipt =>
+                receipt.To<ReceiptProfileViewModel>()).ToList();
+
+            return this.View(receiptsForCurrentUser);
         }
 
         [HttpGet]
